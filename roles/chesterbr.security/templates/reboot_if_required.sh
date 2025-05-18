1027ab684d353ee1208c8bed86321c54195ff5bf
Carlos Duarte Do Nascimento <cd@pobox.com>
2025-05-18 09:08:27 -0400

    add a timeout to server reboot for upgrades

--- roles/chesterbr.security/templates/reboot_if_required.sh
+++ roles/chesterbr.security/templates/reboot_if_required.sh
@@ -7,8 +7,17 @@ if [ -f "/var/run/reboot-required" ]; then
     # Stop all processes managed by supervisorctl
     supervisorctl stop all
 
+    # Initialize timer
+    SECONDS=0
+    TIMEOUT=1800  # 30 minutes in seconds
+
     # Wait until miniTruco has let all players finish their games
     while pgrep -f "java.*server.jar" >/dev/null; do
+        if [ "$SECONDS" -ge "$TIMEOUT" ]; then
+            echo "Timeout reached. Forcing reboot."
+            break
+        fi
+
         echo "Waiting for Java processes to stop..."
         sleep 5
     done
